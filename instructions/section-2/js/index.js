e3b2122afbdcfc9a773d7a9e734da771039d949f
AlmiraKoshkina <almira.koshkina@gmail.com>
2022-10-04 11:28:48 -0400

    Created message forms, added a message list section, prossed message form submissions, and displayed messages in the list.

--- instructions/section-2/js/index.js
+++ instructions/section-2/js/index.js
@@ -17,3 +17,49 @@ for (let i =0; i< skills.length; i++){
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
 }
+
+let messageForm = document.querySelector('[name = "leave_message"]');
+messageForm.addEventListener("submit", (event)=> {
+    
+    event.preventDefault();
+    
+    let textField = document.getElementById('text');
+    let text = textField.value;
+
+    let emailField = document.getElementById('email');
+    let email = emailField.value;
+
+    let messageField = document.getElementById('message');
+    let message = messageField.value;
+
+    console.log (text, email, message);
+
+    let messageSection = document.getElementById('messages');
+    let messageList = messageSection.querySelector('ul');
+    console.log(messageList);
+    let newMessage = document.createElement("li");
+    newMessage.innerHTML = '<a href = "mailto:' + email + '">' + text + '</a> ' + '<span>' + 'wrote: ' +  message +' ' + '</span>';
+    
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML = 'remove';
+    removeButton.type = "button";
+    
+    removeButton.addEventListener('click', () => {
+        let entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.append(removeButton);
+    messageList.append(newMessage);
+
+// if (messageList.value === "none") {
+//     messageSection.style.display = "block";
+
+    document.getElementById('leave_message').reset();
+});
+
+
+
+
+
+
