8b2ec04a904a942f5264cfa4cc817d909bc3f08e
Veronica Johnson <cannon2122@yahoo.com>
2022-10-04 23:25:26 -0400

    Lesson 4-3

--- instructions/section-4/script.js
+++ instructions/section-4/script.js
@@ -6,6 +6,23 @@ const copyright = document.createElement('p');
 copyright.innerHTML = `Veronica M Johnson :${thisyear}`;
 footer.appenChild(copyright);
 
+let messageForm = document.getElementsByName("leaveMessage")
+let SubmitButton = document.getElementsByTagName("button")
+SubmitButton.addEventListener('click', myFunction);
+
+function myFunction(event){
+    event.preventDefault();
+
+    let fname = document.getElementById('name')
+    let emailBox = document.getElementById('email')
+    let messageBox = document.getElementById('message')
+
+    event.target.fname
+    event.target.emailBox
+    event.target.messageBox
+
+    console.log(fname, emailBox,messageBox)
+}
 
 const skills = ['web design', 'HTML', 'CSS','css graphics and animation', 'teaching'];
 const skillSection = document.querySelector('#skills');
